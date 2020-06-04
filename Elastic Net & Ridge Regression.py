ae7d451d0572805420ab5fb5f84c72c4a7ccf190
Christopher Liew <chrisliewjy@gmail.com>
2020-06-04 23:12:47 +0800

    Add files via upload

--- Elastic Net & Ridge Regression.py
+++ Elastic Net & Ridge Regression.py
@@ -5,6 +5,7 @@ from yellowbrick.regressor import AlphaSelection
 from sklearn.linear_model import ElasticNetCV
 from sklearn.linear_model import LassoCV
 from sklearn.linear_model import RidgeCV
+from sklearn.preprocessing import RobustScaler, StandardScaler
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
@@ -16,6 +17,13 @@ train_set = pd.read_csv("train_clean.csv")
 test_set = pd.read_csv("test_clean.csv")
 X = train_set.drop(columns=["Saleprice"])
 y = train_set[["Saleprice"]]
+
+# Scale our Data with Robust Scaler to minimise outlier influence (Approx 4% of the data are significant outliers as measured by Cook's Distance)
+rb_scaler = RobustScaler()
+X_scaled = pd.DataFrame(rb_scaler.fit_transform(X), columns=X.columns)
+
+std_scaler = StandardScaler()
+X_standard = pd.DataFrame(std_scaler.fit_transform(X), columns=X.columns)
 
 ## Define CV Root Mean Square Error ##
 def cv_rmse(estimator, X, y, cv=5):
@@ -29,65 +37,91 @@ elastic_reg = ElasticNetCV(cv=5, max_iter=15000)
 lasso_reg = LassoCV(cv=5, alphas=[0.011], max_iter=15000) # Previously Optimised
 
 ## Model Evaluation & Hyperparameter Tuning ##
-# CV Root Mean Squared Error on Training Set
-cv_rmse(elastic_reg, X, np.ravel(y)) # Elastic Net (ratio = 0.5): 0.4488
-cv_rmse(lasso_reg, X, np.ravel(y)) # LASSO: 0.3193
+# CV Root Mean Squared Error on Training Set (Robust Scaled)
+cv_rmse(lasso_reg, X_scaled, np.ravel(y)) # LASSO: 0.319
+cv_rmse(elastic_reg, X_scaled, np.ravel(y)) # Elastic Net (ratio = 0.5): 0.317
+
+# CV Root Mean Squared Error on Training Set (Standardised)
+cv_rmse(lasso_reg, X_standard, np.ravel(y)) # LASSO: 0.2992
+cv_rmse(elastic_reg, X_standard, np.ravel(y)) # Elastic Net (ratio = 0.5): 0.3012
+
 
 # Alpha Selection
 alphas = np.logspace(-10, 1, 400)
 visualizer = AlphaSelection(elastic_reg)
-visualizer.fit(X, y)
-visualizer.show() # Optimal Alpha = 0.322
+visualizer.fit(X_scaled, y)
+visualizer.show() # Optimal Alpha = 0.020
+
+alphas = np.logspace(-10, 1, 400)
+visualizer = AlphaSelection(elastic_reg)
+visualizer.fit(X_standard, y)
+visualizer.show() # Optimal Alpha = 0.020
 
 # Search Algorithms to Further tune our Hyperparameters
-elastic_reg_tune = ElasticNetCV()
-
 # RandomizedSearchCV to narrow search space
 rnd_params = {"l1_ratio": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
               "alphas": [[0.1], [0.2], [0.3], [0.4], [0.5]],
               "max_iter": [15000],
               "normalize": [False]}
-rnd_src = RandomizedSearchCV(elastic_reg_tune, param_distributions=rnd_params, n_iter=100, scoring="neg_mean_squared_error", n_jobs=-1)
-rnd_src.fit(X, np.ravel(y)) # Convergence Issue
+rnd_src = RandomizedSearchCV(elastic_reg, param_distributions=rnd_params, n_iter=100, scoring="neg_mean_squared_error", n_jobs=-1)
+rnd_src.fit(X_scaled, np.ravel(y))
 rnd_src.best_params_# {'normalize': False, 'max_iter': 15000, 'l1_ratio': 0.1, 'alphas': [0.1]}
-rnd_src.best_score_ # -0.1093 OR MSE = 0.1093
+rnd_src.best_score_ # -0.1117 OR
+
+rnd_src.fit(X_standard, np.ravel(y))
+rnd_src.best_params_# {'normalize': False, 'max_iter': 15000, 'l1_ratio': 0.1, 'alphas': [0.1]}
+rnd_src.best_score_# -0.09379
 
 # GridSearch
 grd_params = {"l1_ratio": [0.1, 0.2, 0.3],
               "alphas": [[0.1], [0.2], [0.3]],
               "max_iter": [30000],
               "normalize": [False]}
-grd_src = GridSearchCV(elastic_reg_tune, param_grid=grd_params, scoring="neg_mean_squared_error", n_jobs=-1)
-grd_src.fit(X, np.ravel(y))
+grd_src = GridSearchCV(elastic_reg, param_grid=grd_params, scoring="neg_mean_squared_error", n_jobs=-1)
+grd_src.fit(X_scaled, np.ravel(y))
+grd_src.best_params_# {'alphas': [0.1], 'l1_ratio': 0.1, 'max_iter': 30000, 'normalize': False}
+grd_src.best_score_ # -0.1117
+
+grd_src.fit(X_standard, np.ravel(y))
 grd_src.best_params_# {'alphas': [0.1], 'l1_ratio': 0, 'max_iter': 30000, 'normalize': False}
-grd_src.best_score_ # -0.0985 OR MSE = 0.0985
+grd_src.best_score_# -0.09379
 
 ## Train & Evaluate Tuned Model ##
 # Test out Alpha Selection from Yellowbrick Module
-elastic_reg = ElasticNetCV(alphas=[0.322], max_iter=15000)
-cv_rmse(elastic_reg, X, np.ravel(y)) # 0.4488, No improvement in RMSE
+elastic_reg_yb = ElasticNetCV(alphas=[0.020], max_iter=15000)
+cv_rmse(elastic_reg_yb, X_scaled, np.ravel(y)) # 0.3191 (Worse) - Scaled
+cv_rmse(elastic_reg_yb, X_standard, np.ravel(y)) # 0.2990 (Better) - Standardised
 
 # Test out GridSearchCV optimal hyperparameters
-elastic_reg_tune = ElasticNetCV(alphas=[0.1], l1_ratio=0, max_iter=15000)
-cv_rmse(elastic_reg_tune, X, y) # Essentially Ridge Regression (l1_ratio = 0): 0.313 (Slight improvement over LASSO)
+elastic_reg_tune = ElasticNetCV(alphas=[0.1], l1_ratio=0, max_iter=30000)
+cv_rmse(elastic_reg_tune, X_scaled, y) # 0.3155 (Slight Improvement, but failed to fully converge) - Scaled
+cv_rmse(elastic_reg_tune, X_standard, y) # 0.3189 - Standardised
 
 ## Ridge Regression ##
 # Since l1_ratio = 0, test out Ridge Regression
-ridge_reg = RidgeCV(alphas=[0.1], scoring="neg_mean_squared_error") # From above
-cv_rmse(ridge_reg, X, y) # 0.329 (worse than LASSO)
-# Ridge due to its Cost Function avoids bouncing around, thus no ConvergenceError vs Lasso/ E-Net
+ridge_reg = RidgeCV(scoring="neg_mean_squared_error") # From above
+cv_rmse(ridge_reg, X_scaled, y) # 0.2995 (Better than LASSO & Elastic Net) - Scaled
+cv_rmse(ridge_reg, X_standard, y) # 0.3298 (Worse than LASSO & Elastic Net) - Standardised
 
 # Tune Ridge Model
-ridge_reg_tune = RidgeCV()
 grd_params_ridge = {'alphas': [[0.1], [0.2], [0.3], [0.4], [0.5], [0.6], [0.7], [0.8], [0.9]]}
-grd_src_ridge = GridSearchCV(ridge_reg_tune, grd_params_ridge, scoring="neg_mean_squared_error")
-grd_src_ridge.fit(X, y) # RunTimeError: Cannot clone object RidgeCV ... (Sklearn bug)
+grd_src_ridge = GridSearchCV(ridge_reg, grd_params_ridge, scoring="neg_mean_squared_error")
+grd_src_ridge.fit(X_scaled, y) # RunTimeError: Cannot clone object RidgeCV ... (Sklearn bug)
 grd_src_ridge.best_params_# {'alphas': [0.9]}
-grd_src_ridge.best_score_ # -0.09862
+grd_src_ridge.best_score_ # -0.09855
+
+grd_src_ridge.fit(X_standard, y) # RunTimeError: Cannot clone object RidgeCV ... (Sklearn bug)
+grd_src_ridge.best_params_# {'alphas': [0.9]}
+grd_src_ridge.best_score_ # -0.1143
 
 # Test our Tuned Ridge Model
 ridge_reg_tune = RidgeCV(alphas=[0.9])
-cv_rmse(ridge_reg_tune, X, y) # 0.3124 (Best score amongst the bunch)
+cv_rmse(ridge_reg_tune, X_scaled, y) # 0.3123 (Worse after tuning, but better than Lasso & Elastic Net)
+cv_rmse(ridge_reg_tune, X_standard, y) # 0.3353 (Worse after tuning, but better than Lasso & Elastic Net)
+
+## Best scores came from an untuned RidgeCV modeL, ridge_reg:  RMSE = 0.2995  AND LASSO: RMSE = 0.2992##
+# LASSO performed better on Standardised Data since our numerical features are already Gaussian via powertransform, but
+# RIDGE performed better on RobustScaled data, since RIDGE is more sensitive to outlier given L2 norm.
 
 ## Further Error Analysis (Learning Curves) ##
 def plot_learning_curves(estimator, X, y):
@@ -108,7 +142,7 @@ def plot_learning_curves(estimator, X, y):
     plt.ylabel("RMSE")
     plt.show()
 
-plot_learning_curves(ridge_reg_tune, X, y)
+plot_learning_curves(ridge_reg_tune, X_scaled, y)
 
 # Relatively low RMSE, and narrow gap between Training & Validation RMSE, thus indicating possible issue of Underfitting (High Var.)
 # Remedies:
